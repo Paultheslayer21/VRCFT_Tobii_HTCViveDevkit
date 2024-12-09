bfc2b2b07792671976a18555d9dd0227d48c1f6a
Paultheslayer21 <58421600+Paultheslayer21@users.noreply.github.com>
2024-12-09 11:14:48 -0500

    Add User Config Json

---
+++ VRCFT-TobiiHTCVive/TobiiConfig.cs
@@ -0,0 +1,19 @@
+﻿public class TobiiConfig
+{
+    public float YOffset
+    {
+        get; set;
+    }
+    public float XOffset
+    {
+        get; set;
+    }
+    public float XNormalization
+    {
+        get; set;
+    }
+    public float YNormalization
+    {
+        get; set;
+    }
+}
